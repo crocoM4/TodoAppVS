e3797af02722b56ac050f0b13ccc02fd318a20d1
crocoM4 <portapozze@libero.it>
2018-01-23 22:36:35 +0100

    Rewrite Category Component
    Rewrite category and button delete category components

---
+++ Scripts/components/ButtonDeleteCategory.jsx
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+const ButtonDeleteCategory = ({ onClick }) => (
+  <button className="button-delete-category" onClick={onClick}>
+    <i className="material-icons">&#xE5CD;</i>
+  </button>
+);
+
+ButtonDeleteCategory.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default ButtonDeleteCategory;
---
+++ Scripts/components/Category.jsx
@@ -0,0 +1,39 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+import ButtonDeleteCategory from './ButtonDeleteCategory';
+
+const Category = ({ category, onClick, onDelete }) => {
+  let cssClass = '';
+
+  if (this.props.isSelezionata) {
+    cssClass = 'category-selected';
+  }
+  return (
+    <div
+      className={`${cssClass} category-chip align-items-center`}
+      onClick={onClick}
+      role="presentation"
+    >
+      <span className="category-tex">{category.name}</span>
+      {
+        (category.id !== '0' && onDelete !== undefined) &&
+          <ButtonDeleteCategory onClick={onDelete} />
+      }
+    </div>
+  );
+};
+
+Category.propTypes = {
+  onDelete: PropTypes.func,
+  onClick: PropTypes.func.isRequired,
+  category: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+Category.defaultProps = {
+  onDelete: undefined,
+};
+
+export default Category;
