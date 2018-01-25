441e49e4b6a6961b82dda1a2e4101352cd706f57
crocoM4 <portapozze@libero.it>
2018-01-25 22:40:20 +0100

    Rewrite arguments component
    Rewrite arguments component

---
+++ Scripts/components/Arguments.jsx
@@ -0,0 +1,30 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import Argument from './Argument';
+
+const Arguments = ({ todoArguments, onDeleteArgument }) => (
+  <div id="content-arguments-todo">
+    {
+      todoArguments.map(arg => (
+        <Argument
+          key={arg.id}
+          argument={arg}
+          onDelete={() => onDeleteArgument(arg)}
+        />
+      ))
+    }
+  </div>
+);
+
+Arguments.propTypes = {
+  onDeleteArgument: PropTypes.func.isRequired,
+  todoArguments: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    completed: PropTypes.string.isRequired,
+    category: PropTypes.shape({}).isRequired,
+  }).isRequired).isRequired,
+};
+
+export default Arguments;
