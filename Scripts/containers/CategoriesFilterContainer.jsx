21de3de4b038e3909059e78c595bcf864b54a5ca
crocoM4 <portapozze@libero.it>
2018-01-27 21:51:33 +0100

    Rewrite container categories
    Rewrite container categories

---
+++ Scripts/containers/CategoriesFilterContainer.jsx
@@ -0,0 +1,33 @@
+﻿import { connect } from 'react-redux';
+import CategoriesFilter from '../components/CategoriesFilter';
+import * as action from '../actions';
+
+const mapStateToProps = state => (
+  {
+    categoriesFilterList: state.categories.items,
+  }
+);
+
+const mapDispatchToProps = dispatch => (
+  {
+    onDeleteCategory: (category) => {
+      dispatch(action.deleteCategory(category));
+    },
+    onCilckCategory: (category, e) => {
+      if (e.target.tagName.toLowerCase() !== 'i' && e.target.tagName.toLowerCase() !== 'button') {
+        if (category.Id === '0') {
+          dispatch(action.selectCategoryAll());
+        } else {
+          dispatch(action.selectCategory(category));
+        }
+      }
+    },
+  }
+);
+
+const CategoriesFilterContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(CategoriesFilter);
+
+export default CategoriesFilterContainer;
