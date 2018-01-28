e506c00eef884c341fa613b6ff27db519fc28089
crocoM4 <portapozze@libero.it>
2018-01-28 11:26:30 +0100

    Rewrite main add button
    Rewrite main add button

---
+++ Scripts/components/MainAddButton.jsx
@@ -0,0 +1,29 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import * as action from '../actions';
+
+const createHandlers = (dispatch) => {
+  const onClick = () => {
+    action.toogleDialog(dispatch);
+  };
+  return {
+    onClick,
+  };
+};
+
+const MainAddButton = ({ dispatch }) => {
+  const handlers = createHandlers(dispatch);
+  return (
+    <button id="main-add-button" onClick={handlers.onClick} >
+      <i className="material-icons">&#xE145;</i>
+    </button>
+  );
+};
+
+MainAddButton.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect()(MainAddButton);
