6ecae12007fa60a94621e309ae07cb13099d8da7
crocoM4 <portapozze@libero.it>
2018-03-04 10:44:52 +0100

    Change button delete  argument  style
    Change button delete argument style

--- src/components/ButtonDeleteArgument.jsx
+++ src/components/ButtonDeleteArgument.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 const ButtonDeleteArgument = ({ onClick }) => (
   <button className="button-delete-argument" onClick={onClick}>
-    <i className="material-icons">&#xE5CD;</i>
+    <i className="icon-ic-delete" />
   </button>
 );
 
