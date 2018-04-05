6f9c6836affdc51c29e80956b91095f88e5556e3
crocoM4 <portapozze@libero.it>
2018-04-05 23:00:22 +0200

    Renamed button arrow to scroll
    Renamed button arrow to scroll

--- src/components/ButtonScoll.jsx
+++ src/components/ButtonScoll.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ButtonArrow = ({ onClick, direction }) => (
-  <button className="button-arrow" onClick={onClick}>
+const ButtonScroll = ({ onClick, direction }) => (
+  <button className="button-scroll" onClick={onClick}>
     <i className={(direction === 'left') ? 'icon-backward' : 'icon-forward'} />
   </button>
 );
 
-ButtonArrow.propTypes = {
+ButtonScroll.propTypes = {
   onClick: PropTypes.func.isRequired,
   direction: PropTypes.oneOf(['left', 'right']),
 };
 
-ButtonArrow.defaultProps = {
+ButtonScroll.defaultProps = {
   direction: 'left',
 };
 
-export default ButtonArrow;
+export default ButtonScroll;
